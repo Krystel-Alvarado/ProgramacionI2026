ad3b6ba32e2b6be08764fe2b087b24f31530a4ad
Krystel-Alvarado <krystelalvaradou2025@gmail.com>
2026-02-19 23:07:22 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿// Mi primer proyecto C#
+Console.WriteLine("Hola Mundo!");
+
+string minombre = "Krystel Alvarado";
+int miedad = 19;
+
+
+Console.WriteLine("Nombre alumna: " + minombre+ "\n" + "Edad: " + miedad+ " años");
+
+/*Prueba de comentario en bloque
+Prueba de comentario en bloque
+Prueba de comentario en bloque
+Prueba de comentario en bloque
+Prueba de comentario en bloque
+Prueba de comentario en bloque
+Prueba de comentario en bloque*/
+
+//operadores
+int valor1 = 80, valor2 = 50, valor3 = 10;
+//suma
+int total= valor1 + valor2;
+Console.WriteLine("Total de la suma: " + total);
+
+//resta
+int diferencia = valor1 - valor2;
+Console.WriteLine("Total de la resta: " + diferencia);
+
+//multiplicacion
+int producto = valor1 * valor3;
+Console.WriteLine("Resultado: " + producto);
+
+//division
+int resultado = valor1 / valor3;
+Console.WriteLine("Resultado: " + resultado);
+
+//residuo
+var modulo = valor2 % valor3;
+Console.WriteLine("Residuo: " + modulo);
+
+
+
+
+
+Console.ReadKey();
